cf889923dd9bce27eb5bc6c94007d644b17c13e5
thiagojcosme <thiago_junio3@hotmail.com>
2024-03-22 15:24:39 -0300

    envio

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const API_URL = `https://v6.exchangerate-api.com/v6/${token}/latest`
 export function getImporteData(moeda){
     return fetch(`${API_URL}/${moeda}`)
     .then(response => response.json())
-    .then(data => Object.entries(data).map((name, value) => name, value));
+    .then(data => Object.entries(data.conversion_rates).map(([name, value]) => (name, value )))
     //     response.json().then((data) => {
     //         return data; 
             
